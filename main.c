071674011cc31e9530190c2a5df078439f0392d3
Jorge Parga <parga@adobe.com>
2025-02-05 15:07:42 +0100

    first test up and running
    ignore o files
    
    also ignore .out files
    
    ignore my_program since is the default

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
+  int sneklang_default_max_threads = 8;
+  char sneaklang_default_perms = 'r';
+  float sneaklang_default_pi = 3.14159;
+  char *title = "sneaklang";
+
+  printf("Default max threads: %d\n", sneklang_default_max_threads);
+  printf("Custom perms %c\n", sneaklang_default_perms);
+  printf("Constant pi value %f\n", sneaklang_default_pi);
+  printf("Sneaklang title %s\n", title);
 
   return 0;
 }
