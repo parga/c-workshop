7599039aea3217cb894a8f031c53d2dc8c1a0d08
Jorge Parga <parga@adobe.com>
2025-02-05 15:27:10 +0100

    fix issues in the wathch

--- watch.sh
+++ watch.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 read -p 'Which c file to watch?: ' file_name
-read -p 'Which gcc flags? Type them all with a space between: ' opt_flags
 
-old_file_sig=$(stat $file_name)
+old_file_sig=$(stat "$file_name")
 echo "Watching $file_name for changes..."
 while true
 do
-  new_file_sig=$(stat $file_name)
+  new_file_sig=$(stat "$file_name")
   if [[ "$new_file_sig" != "$old_file_sig" ]]; then
         echo ""
         echo "$file_name changed... compiling and running"
         echo ""
-        clang $file_name -o "main"
+        clang "$file_name" -o "main"
         old_file_sig=$new_file_sig
         echo ""
         echo "Watching $file_name for changes..."
